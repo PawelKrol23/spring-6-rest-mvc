8c13b01264f1bf7908fb46b9d6b6a18dd03c92b6
PawelKrol23 <p.krol18a@gmail.com>
2023-03-12 17:38:04 +0100

    Created mappers to Beer and Customer

---
+++ src/main/java/com/example/spring6restmvc/mappers/CustomerMapper.java
@@ -0,0 +1,11 @@
+package com.example.spring6restmvc.mappers;
+
+import com.example.spring6restmvc.entities.Customer;
+import com.example.spring6restmvc.model.CustomerDTO;
+import org.mapstruct.Mapper;
+
+@Mapper
+public interface CustomerMapper {
+    Customer customerDtoToCustomer(CustomerDTO DTO);
+    CustomerDTO customerToCustomerDto(Customer customer);
+}
