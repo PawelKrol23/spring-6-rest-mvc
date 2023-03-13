563d265f6b8a250feed88b9d29b7eb11be0fea4d
PawelKrol23 <p.krol18a@gmail.com>
2023-03-13 12:35:40 +0100

    Created BeerServiceJPA and CustomerServiceJPA

---
+++ src/main/java/com/example/spring6restmvc/services/CustomerServiceJPA.java
@@ -0,0 +1,48 @@
+package com.example.spring6restmvc.services;
+
+import com.example.spring6restmvc.mappers.CustomerMapper;
+import com.example.spring6restmvc.model.CustomerDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+@Primary
+@RequiredArgsConstructor
+public class CustomerServiceJPA implements CustomerService {
+    private final CustomerService customerService;
+    private final CustomerMapper customerMapper;
+    @Override
+    public List<CustomerDTO> listCustomers() {
+        return null;
+    }
+
+    @Override
+    public Optional<CustomerDTO> getCustomerById(UUID id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public CustomerDTO saveCustomer(CustomerDTO customer) {
+        return null;
+    }
+
+    @Override
+    public void updateCustomer(UUID customerId, CustomerDTO customer) {
+
+    }
+
+    @Override
+    public void deleteCustomer(UUID customerId) {
+
+    }
+
+    @Override
+    public void patchCustomer(UUID customerId, CustomerDTO customer) {
+
+    }
+}
